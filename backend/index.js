3c38dade3c929256d7b79113ee9d4d6aa2061aa6
pothinaharika <harikapothina1902@gmail.com>
2026-04-17 14:45:48 +0530

    modified changes

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const cors = require("cors")
 require("dotenv").config()
 const authRoutes=require("./routes/authRoutes")
 const productRoutes=require("./routes/productRoutes")
+const cartRoutes=require("./routes/cartRoutes")
 
 app.use(cors())
 app.use(express.json())
@@ -24,4 +25,5 @@ app.get("/",(req,res)=>{
 })
 app.use("/api/auth",authRoutes)
 app.use("/api/product",productRoutes)
+app.use("/api/cart",cartRoutes)
 app.listen(5000,()=>console.log("server runns on port 5000"))
